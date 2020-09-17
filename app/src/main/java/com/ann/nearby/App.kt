1adc4f4e02d7b29272b9ea45f14d4a2f1f8f1947
Ann <>
2020-09-17 23:20:27 +0800

    Implement Mapbox and add MapView
    Implement Mapbox and add MapView

--- app/src/main/java/com/ann/nearby/App.kt
+++ app/src/main/java/com/ann/nearby/App.kt
@@ -2,6 +2,7 @@ package com.ann.nearby
 
 import android.app.Application
 import com.ann.nearby.di.module.viewModelModule
+import com.mapbox.mapboxsdk.Mapbox
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -11,6 +12,18 @@ class App:Application() {
         startKoin{
             androidContext(this@App)
             modules(listOf(viewModelModule))
+        }
+
+        Mapbox.getInstance(
+            this,
+            BuildConfig.MAPBOX_ACCESS_TOKEN
+        )
+        if (BuildConfig.DEBUG) {
+            if (Mapbox.getTelemetry() == null) {
+                throw RuntimeException("Mapbox.getTelemetry() == null in debug config")
+            } else {
+                Mapbox.getTelemetry()!!.setDebugLoggingEnabled(true)
+            }
         }
     }
 }
--- app/src/main/java/com/ann/nearby/ui/main/MainFragment.kt
+++ app/src/main/java/com/ann/nearby/ui/main/MainFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ann.nearby.R
+import com.mapbox.mapboxsdk.maps.Style
+import kotlinx.android.synthetic.main.main_fragment.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
@@ -19,6 +21,46 @@ class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync { mapboxMap ->
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
     }
 
 }
